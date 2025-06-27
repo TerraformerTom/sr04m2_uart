5e809f598bcb7a08a27e6b9f3543b79c5eba4437
TerraformerTom <30509676+TerraformerTom@users.noreply.github.com>
2025-06-28 00:07:38 +0200

    Update sensor.py

--- esphome/components/sr04m2_uart/sensor.py
+++ esphome/components/sr04m2_uart/sensor.py
@@ -1,28 +1,25 @@
-#include "esphome/core/component.h"
-#include "esphome/components/uart/uart.h"
-#include "esphome/components/sensor/sensor.h"
+import esphome.codegen as cg
+import esphome.config_validation as cv
+from esphome.components import uart, sensor
+from esphome.const import UNIT_CENTIMETER, ICON_WATER, DEVICE_CLASS_DISTANCE, STATE_CLASS_MEASUREMENT
 
-namespace esphome {
-namespace sr04m2_uart {
+sr04m2_uart_ns = cg.esphome_ns.namespace('sr04m2_uart')
+SR04M2UARTSensor = sr04m2_uart_ns.class_('SR04M2UARTSensor', sensor.Sensor, cg.PollingComponent, uart.UARTDevice)
 
-class SR04M2UARTSensor : public sensor::Sensor, public Component, public uart::UARTDevice {
- public:
-  SR04M2UARTSensor(uart::UARTComponent *parent) : uart::UARTDevice(parent) {}
+CONFIG_SCHEMA = sensor.sensor_schema(
+    unit_of_measurement=UNIT_CENTIMETER,
+    icon=ICON_WATER,
+    accuracy_decimals=1,
+    device_class=DEVICE_CLASS_DISTANCE,
+    state_class=STATE_CLASS_MEASUREMENT,
+).extend({
+    cv.GenerateID(): cv.declare_id(SR04M2UARTSensor),
+    cv.GenerateID('uart_id'): cv.use_id(uart.UARTComponent),
+    cv.Optional("update_interval", default="5s"): cv.update_interval
+}).extend(cv.polling_component_schema("5s"))
 
-  void loop() override {
-    while (this->available()) {
-      if (this->read() == 0xFF && this->available() >= 3) {
-        uint8_t high = this->read();
-        uint8_t low  = this->read();
-        uint8_t sum  = this->read();
-        if (sum == ((0xFF + high + low) & 0xFF)) {
-          float dist_cm = ((high << 8) | low) / 10.0f;
-          publish_state(dist_cm);
-        }
-      }
-    }
-  }
-};
-
-}  // namespace sr04m2_uart
-}  // namespace esphome
+async def to_code(config):
+    var = cg.new_Pvariable(config[cv.ID], config[cv.CONF_UPDATE_INTERVAL])
+    await cg.register_component(var, config)
+    await sensor.register_sensor(var, config)
+    await uart.register_uart_device(var, config)
